0f65d7e7a51cf8555798a585c3a9b40ff7016417
Beatris Macovei <beatriz1308m.m@gmail.com>
2025-11-24 18:35:07 +0100

    Fix logic for counting positive and negative numbers

--- NumerosExamenWhile.java
+++ NumerosExamenWhile.java
@@ -4,8 +4,11 @@ public numbers()
 	int numero = teclado.nextInt()
 	while(numero!=0)
 	{
-		positivo=positivo+numero;
-        	contadorPositivo++;
+		if(numero>0)
+     	{
+        positivo=positivo+numero;
+        contadorPositivo++;
+      	}
 		if(numero<0)
 		{
 			negativos=negativos+numero;
@@ -18,4 +21,4 @@ public numbers()
    	ssystem.out("media="+ positivos/contadorPositivos);
    	if(contadorNegativos>0)
    	System.out.print("media ="+ negativos/contadorNegativos);
-}
+
