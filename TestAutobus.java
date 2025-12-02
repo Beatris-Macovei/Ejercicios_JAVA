c32435d53ccc23601dab85fdca4fa968cc1139ca
Beatris Macovei <beatriz1308m.m@gmail.com>
2025-12-02 19:28:05 +0100

    Refactor output messages in TestAutobus

--- TestAutobus.java
+++ TestAutobus.java
@@ -3,15 +3,13 @@ public class TestAutobus
   public static void main(String[]args)
   {
     Autobus inter = new Autobus();
+
+     System.out.println("Estas son las plazas ocupadas"+plazasOcupadas());
     
-    inter.plazasOcupadas();
-     System.out.println("Estas son las plazas ocupadas");
-    
-    inter.plazasLibres();
-     System.out.println("Estas son las plazas libres");
+     System.out.println("Estas son las plazas libres"+plazasLibres());
      
     inter.personaAsiento();
-     System.out.println("Esta es la plaza que has icho");
+     System.out.println("Esta es la plaza que has elegido");
      
     inter.subirPersona();
      System.out.println("Esta plaza se ha ocupado");
